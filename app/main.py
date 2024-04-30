d2e39a929ee7dc61182b7e0ed00b3846368d67fa
Ng Heng Khai <nhkhai@gmail.com>
2024-04-30 10:57:53 +0800

    Renamed the app name.

--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ from .api.main import api_router
 from .core.config import settings
 
 
-the_review_room = FastAPI()
+app = FastAPI()
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
-    the_review_room.add_middleware(
+    app.add_middleware(
         CORSMiddleware,
         allow_origins=[
             str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
@@ -20,12 +20,12 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
-the_review_room.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 # Initiate the database, create the tables and load the initial data. 
 start_db.main()
 dataloader.main()
 
-@the_review_room.get("/")
+@app.get("/")
 async def root():
     return {"message": "Hello World"}
--- app/start.bat
+++ app/start.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
 PUSHD ..
-@REM uvicorn app.main:the_review_room --reload
-@REM uvicorn app.main:the_review_room --host 0.0.0.0 --port 80
-@REM uvicorn app.main:the_review_room --host 0.0.0.0 --port 8000 --reload
-uvicorn app.main:the_review_room --host 0.0.0.0 --port 80 --reload
+@REM uvicorn app.main:app --reload
+@REM uvicorn app.main:app --host 0.0.0.0 --port 80
+@REM uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+@REM uvicorn app.main:app --host 0.0.0.0 --port 80 --reload
+uvicorn app.main:app --host 0.0.0.0 --port 9000 --reload
 POPD
 @REM PAUSE
